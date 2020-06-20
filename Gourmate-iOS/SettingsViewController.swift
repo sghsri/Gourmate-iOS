66012a2c86d6bb560dbb9fbfbd88af379486e2fd
jsuriadinata <jsuriadinata@utexas.edu>
2020-06-20 10:32:34 -0500

    Modify existing screens

---
+++ Gourmate-iOS/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  Gourmate-iOS
+//
+//  Created by Matthew Onghai on 6/18/20.
+//  Copyright © 2020 utexas. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Gourmate-iOS/SignInViewController.swift
+++ Gourmate-iOS/SignInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInViewController.swift
 //  Gourmate-iOS
 //
 //  Created by Sriram Hariharan on 6/13/20.
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Change all screens to dark mode
+        UIApplication.shared.windows.forEach { window in
+            window.overrideUserInterfaceStyle = .dark
+        }
+        
         // Do any additional setup after loading the view.
     }
 
