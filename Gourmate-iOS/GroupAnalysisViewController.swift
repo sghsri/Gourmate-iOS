d3c0d86cb3a763cc49636b9698d4166778c3c83f
jsuriadinata <jsuriadinata@utexas.edu>
2020-07-05 17:07:09 -0500

    Add dietary restriction analysis

--- Gourmate-iOS/GroupAnalysisViewController.swift
+++ Gourmate-iOS/GroupAnalysisViewController.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
-class GroupAnalysisViewController: UIViewController {
+class DietaryGroupAnalysis: UITableViewCell {
     
+    @IBOutlet weak var dietaryRestrictionText: UILabel!
+    @IBOutlet weak var checkmark: UIImageView!
+}
+
+class GroupAnalysisViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var dietaryRestrictionTable: UITableView!
     @IBOutlet weak var cuisineAnalysisView: UIView!
     
+    let dietaryRestrictions = ["Vegetarian", "Vegan"]
+    
+    // Create bar chart
     lazy var barChartView: BarChartView = {
         let barChartView = BarChartView()
         barChartView.frame = view.frame
@@ -22,8 +32,13 @@ class GroupAnalysisViewController: UIViewController {
      override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set dietary restrictions table delegate and data source
+        dietaryRestrictionTable.delegate = self
+        dietaryRestrictionTable.dataSource = self
+        
         let cuisines = ["American", "Chinese", "Mexican", "Thai", "Japanese", "Indian"] // Types of cuisine
         
+        // Create cuisine preferences bar chart
         barChartView.dataEntries =
            [
               BarEntry(score: 1, title: cuisines[0]),
@@ -33,6 +48,21 @@ class GroupAnalysisViewController: UIViewController {
               BarEntry(score: 2, title: cuisines[4]),
               BarEntry(score: 2, title: cuisines[5])
            ]
-        cuisineAnalysisView.addSubview(barChartView)
+        cuisineAnalysisView.addSubview(barChartView) // Add to VC
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dietaryRestrictions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dietaryRestrictionCell", for: indexPath) as! DietaryGroupAnalysis
+        
+        cell.dietaryRestrictionText.text = dietaryRestrictions[indexPath.row]
+        cell.checkmark.frame = CGRect(x: 300, y: 20, width: 15, height: 15)
+        
+        cell.checkmark.image = UIImage.init(named: "checkmark")
+        
+        return cell
     }
 }
